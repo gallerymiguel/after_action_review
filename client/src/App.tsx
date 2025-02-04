f52939c59ffa75a839785d3fdd3bdafa628c5dde
Spider Chai <176632137+CelestialChai@users.noreply.github.com>
2025-02-03 19:44:14 -0600

    saving changes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,10 @@ import React from "react";
 import { CssBaseline, Box, Container } from "@mui/material";
 import { Outlet, useLocation } from "react-router-dom";
 import NavigationBar from "./components/nav";
+import MissionForm from './components/mission_form';
+
+import "./App.css";
+
 
 // Debug Component to Log Current Location
 const DebugLocation: React.FC = () => {
