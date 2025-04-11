739d97c4e41de73657e5ba5982fb7b62076fe47b
Miguel Urdiales <galleriesproductions@gmail.com>
2025-04-10 20:02:35 -0400

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,32 +1,9 @@
-import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from "@apollo/client";
-import { setContext } from "@apollo/client/link/context";
+import React from "react";
 import { CssBaseline, Box, Container } from "@mui/material";
 import { Outlet, useLocation } from "react-router-dom";
 import NavigationBar from "./components/nav";
 import "./App.css";
 
-// Create HTTP link for Apollo Client
-const httpLink = createHttpLink({
-  uri: "/graphql",
-  credentials: "include", // Allows cookies and authorization headers
-});
-
-// Set Auth Context for Apollo Client
-const authLink = setContext((_, { headers }) => {
-  const token = localStorage.getItem("id_token");
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : "",
-    },
-  };
-});
-
-// Initialize Apollo Client
-const client = new ApolloClient({
-  link: authLink.concat(httpLink),
-  cache: new InMemoryCache(),
-});
 
 // Debug Component to Log Current Location
 const DebugLocation: React.FC = () => {
@@ -37,30 +14,30 @@ const DebugLocation: React.FC = () => {
 
 const App: React.FC = () => {
   return (
-    <ApolloProvider client={client}>
+    <>
       <CssBaseline />
       <NavigationBar />
       <Box
         sx={{
           display: "flex",
           flexDirection: "column",
-          minHeight: "100vh", // ✅ minHeight instead of height
-          overflow: "auto", // ✅ allow scrolling
+          height: "100vh", // Limit total height to prevent scrolling
+          overflow: "hidden", // Prevents unnecessary scrollbars
         }}
       >
         {/* Main Content Area */}
         <Container
-          maxWidth={false}
+          maxWidth={false} 
           sx={{
-            width: "100vw",
-            maxWidth: "100%",
+            width: "100vw", 
+            maxWidth: "100%", 
             flexGrow: 1,
             display: "flex",
             flexDirection: "column",
             alignItems: "center",
             justifyContent: "center",
             textAlign: "center",
-            px: 0,
+            px: 0, 
           }}
         >
           <DebugLocation />
@@ -73,14 +50,14 @@ const App: React.FC = () => {
           sx={{
             width: "100vw",
             backgroundColor: "#f5f5f5",
-            py: 2,
+            py: 2, 
             textAlign: "center",
           }}
         >
           <Container maxWidth="lg">© {new Date().getFullYear()} AAR Platform</Container>
         </Box>
       </Box>
-    </ApolloProvider>
+    </>
   );
 };
 
